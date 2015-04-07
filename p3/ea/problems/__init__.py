3a67724ed32ed3a880ba812d647c3e2d8af90bb5
Øyvind Robertsen <oyvind@arcticstyle.net>
2015-04-07 13:33:45 +0200

    missing files

---
+++ p3/ea/problems/__init__.py
@@ -0,0 +1,28 @@
+class Problem(object):
+
+    def fitness(self, phenotype):
+        '''
+        Returns a float between 0 and 1, 1 is optimal solution
+        '''
+        raise NotImplementedError("Must be implemented by Problem subclass.")
+
+    def create_initial_population(self, population_size):
+        '''
+        Returns an iterable of Individuals
+        '''
+        raise NotImplementedError("Must be implemented by Problem subclass.")
+
+    def geno_to_pheno(self, genotype):
+        '''
+        Converts a genotype to a phenotype
+        '''
+        raise NotImplementedError("Must be implemented by Problem subclass.")
+
+    def mutate_genome_component(self, component):
+        '''
+        Mutates a single genome component
+        '''
+        raise NotImplementedError("Must be implemented by Problem subclass.")
+
+    def __repr__(self):
+        return self.__class__.__name__
