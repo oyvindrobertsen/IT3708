0e0ad2fe1b95a6a9bd51c3449559b4d164ffc324
Mathias Ose <mathias.ose@gmail.com>
2015-04-07 18:18:54 +0200

    sensors for the agent

--- p3/flatland.py
+++ p3/flatland.py
@@ -4,16 +4,12 @@ from random import random as r, choice
 from utils import tuple_add
 
 
-NORTH = 'N'
-WEST = 'W'
-SOUTH = 'S'
-EAST = 'E'
-AGENT_DIRECTIONS = (NORTH, WEST, SOUTH, EAST)
+AGENT_DIRECTIONS = NORTH, WEST, SOUTH, EAST = 'N', 'W', 'S', 'E'
 DELTAS = {
-    NORTH: (-1, 0),
-    WEST: (0, -1),
-    SOUTH: (1, 0),
-    EAST: (0, 1)
+    NORTH: (0, -1),
+    WEST: (-1, 0),
+    SOUTH: (0, 1),
+    EAST: (1, 0)
 }
 
 FOOD = 'F'
@@ -42,21 +38,35 @@ class FlatlandProblem:
     def get_tile(self, x, y):
         return self.grid[y][x]
 
+    def abs_coords(self, x, y):
+        return x % self.width, y % self.height
+
     @property
     def agent_position(self):
         return self.agent_x, self.agent_y
 
-    def eat_food_handler(self):
-        pass  # TODO
+    @property
+    def left_direction(self):
+        return AGENT_DIRECTIONS[AGENT_DIRECTIONS.index(self.agent_heading) - 1]
 
-    def eat_poison_handler(self):
-        pass  # TODO
+    @property
+    def right_direction(self):
+        return AGENT_DIRECTIONS[(AGENT_DIRECTIONS.index(self.agent_heading) + 1) % len(AGENT_DIRECTIONS)]
+
+    @property
+    def forward_coordinate(self):
+        return self.abs_coords(*tuple_add(self.agent_position, DELTAS[self.agent_heading]))
+
+    @property
+    def left_coordinate(self):
+        return self.abs_coords(*tuple_add(self.agent_position, DELTAS[self.right_direction]))
+
+    @property
+    def right_coordinate(self):
+        return self.abs_coords(*tuple_add(self.agent_position, DELTAS[self.left_direction]))
 
     def agent_forward(self):
-        self.agent_x, self.agent_y = tuple_add(self.agent_position, DELTAS[self.agent_heading])
-
-        self.agent_x %= self.width
-        self.agent_y %= self.height
+        self.agent_x, self.agent_y = self.forward_coordinate
 
         if self.get_tile(self.agent_x, self.agent_y) == FOOD:
             self.eat_food_handler()
@@ -66,7 +76,20 @@ class FlatlandProblem:
         self.grid[self.agent_y][self.agent_x] = EMPTY
 
     def agent_turn_left(self):
-        self.agent_heading = AGENT_DIRECTIONS[AGENT_DIRECTIONS.index(self.agent_heading) - 1]
+        self.agent_heading = self.left_direction
 
     def agent_turn_right(self):
-        self.agent_heading = AGENT_DIRECTIONS[(AGENT_DIRECTIONS.index(self.agent_heading) + 1) % len(AGENT_DIRECTIONS)]
+        self.agent_heading = self.right_direction
+
+    def get_sensor_readings(self):
+        return {
+            'forward': self.get_tile(*self.forward_coordinate),
+            'left': self.get_tile(*self.left_coordinate),
+            'right': self.get_tile(*self.right_coordinate)
+        }
+
+    def eat_food_handler(self):
+        pass  # TODO
+
+    def eat_poison_handler(self):
+        pass  # TODO
