6888355246263f464ca23dee00bdb69cc8bb3af2
Mathias Ose <mathias.ose@gmail.com>
2015-04-09 19:19:22 +0200

    config that works pretty good

--- p3/main.py
+++ p3/main.py
@@ -5,22 +5,22 @@ from ea.ea import EARunner
 from ea.problems.utils import *
 
 if __name__ == "__main__":
-    layers = [6, 4, 3]
-    bias = {}
+    layers = [6, 6, 5, 3]
+    bias = {2: [1.0]}
     problem = EvoFlatland(8, layers, bias)
 
     # Configure the runner
-    population_size = 20
-    generations = 10
+    population_size = 200
+    generations = 100
     crossover_rate = 0.80
     mutation_rate = 0.01
-    adult_selection = full_replacement
+    adult_selection = generational_mixing
     adult_to_child_ratio = 0.5
-    parent_selection = tournament_selection
+    parent_selection = sigma_scaling_selection
     k = 8
     epsilon = 0.05
     crossover_function = one_point_crossover
-    mutation_function = per_genome
+    mutation_function = per_genome_component
     threshold = 1.0
 
     runner1 = EARunner(
